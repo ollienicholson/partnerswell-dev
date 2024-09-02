a543aeaa367fb02a3210eb621a99c2d45f4a36a6
Ollie <ollie.nicholson@gmail.com>
2024-09-02 11:48:42 +1000

    delete account component, edit account component

---
+++ src/app/components/DeleteAlertBox.tsx
@@ -0,0 +1,73 @@
+"use client";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogHeader,
+  AlertDialogTitle,
+} from "~/app/components/ui/alert-dialog";
+import { Button } from "~/app/components/ui/button";
+import { useParams, useRouter } from "next/navigation";
+import { api } from "~/trpc/react";
+import { useState } from "react";
+
+export function DeleteAlertBox({ children }: { children: React.ReactNode }) {
+  const router = useRouter();
+  const { accountId: partnerAccountId } = useParams();
+  const [accountIdToDelete] = useState(
+    partnerAccountId ? Number(partnerAccountId) : null,
+  );
+
+  // delete partner account mutation
+  const partnerAccountMutation =
+    api.partnerAccountRouter.deletePartnerAccount.useMutation({
+      onSuccess: () => {
+        router.push("/partner-accounts");
+      },
+      onError: (error) => {
+        console.error("Error deleting partner account:", error);
+      },
+    });
+
+  const handleAccountDelete = async () => {
+    if (!accountIdToDelete)
+      return console.warn("Account ID is missing or invalid");
+
+    try {
+      await partnerAccountMutation.mutateAsync({
+        partnerAccountId: Number(accountIdToDelete),
+      });
+    } catch (error) {
+      console.error("Error during deletion:", error);
+    }
+  };
+
+  return (
+    <AlertDialog>
+      {children}
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+          <AlertDialogDescription>
+            This action cannot be undone. This will permanently delete your
+            account and remove your data from our servers.
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <div className="mt-4 flex justify-between">
+          <AlertDialogCancel>Cancel</AlertDialogCancel>
+          <AlertDialogAction asChild>
+            <Button
+              type="button"
+              variant="destructive"
+              onClick={handleAccountDelete}
+            >
+              Delete
+            </Button>
+          </AlertDialogAction>
+        </div>
+      </AlertDialogContent>
+    </AlertDialog>
+  );
+}
---
+++ src/app/components/EditAccountButton.tsx
@@ -0,0 +1,124 @@
+"use client";
+
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+} from "~/app/components/ui/dialog";
+
+import { AlertDialogTrigger } from "~/app/components/ui/alert-dialog";
+
+import { Input } from "~/app/components/ui/input";
+import { Label } from "~/app/components/ui/label";
+import { Button } from "~/app/components/ui/button";
+import { DeleteAlertBox } from "~/app/components/DeleteAlertBox";
+import { api } from "~/trpc/react";
+import { useParams } from "next/navigation";
+import { useQueryClient } from "@tanstack/react-query"; // Import useQueryClient
+import { useState } from "react";
+
+// look into use of accountContact
+export function EditAccountButton({
+  accountName,
+  accountContact,
+}: {
+  accountName: string;
+  accountContact: string;
+}) {
+  const [isOpen, setIsOpen] = useState(false);
+  const queryClient = useQueryClient();
+  const { accountId } = useParams();
+
+  const [accountNameToUpdate, setaccountNameToUpdate] = useState("");
+  const [contactToUpdate, setcontactToUpdate] = useState("");
+
+  // update partner account
+  const updatePartnerAccountMutation =
+    api.partnerAccountRouter.updatePartnerAccount.useMutation({
+      onSuccess: () => {
+        queryClient.invalidateQueries();
+      },
+      onError: (error) => {
+        console.error("Error updating partner account:", error);
+      },
+    });
+
+  const handleAccountUpdate = async () => {
+    if (!accountId) return console.warn("Account ID is missing or invalid");
+
+    try {
+      await updatePartnerAccountMutation.mutateAsync({
+        partnerAccountId: Number(accountId),
+        accountName: accountNameToUpdate,
+        contactName: contactToUpdate,
+      });
+      setIsOpen(false);
+    } catch (error) {
+      console.error("Error updating partner account:", error);
+    }
+  };
+
+  return (
+    <>
+      <Button onClick={() => setIsOpen(true)}>Edit Account</Button>
+      <Dialog open={isOpen} onOpenChange={setIsOpen}>
+        <DialogContent className="sm:max-w-[600px]">
+          <DialogHeader>
+            <DialogTitle>Edit {accountName}'s account</DialogTitle>
+            <DialogDescription>
+              Save Changes will update the account details. Selecting Delete
+              Account will first ask you if you're 100% sure.
+            </DialogDescription>
+          </DialogHeader>
+          <div className="grid gap-4 py-4">
+            <div className="grid grid-cols-4 items-center gap-4">
+              <Label htmlFor="accountName" className="text-right">
+                Account Name
+              </Label>
+              <Input
+                id="accountName"
+                value={accountNameToUpdate}
+                onChange={(e) => setaccountNameToUpdate(e.target.value)}
+                placeholder="Partner account name"
+                className="col-span-3"
+              />
+            </div>
+            <div className="grid grid-cols-4 items-center gap-4">
+              <Label htmlFor="contactName" className="text-right">
+                Primary Contact
+              </Label>
+              <Input
+                id="contactName"
+                value={contactToUpdate}
+                onChange={(e) => setcontactToUpdate(e.target.value)}
+                placeholder="Contact's full name"
+                className="col-span-3"
+              />
+            </div>
+          </div>
+          <div className="flex justify-between">
+            <Button
+              variant="secondary"
+              type="button"
+              onClick={() => setIsOpen(false)}
+            >
+              Cancel
+            </Button>
+            <Button type="button" onClick={handleAccountUpdate}>
+              Save Changes
+            </Button>
+            <DeleteAlertBox>
+              <AlertDialogTrigger asChild>
+                <Button variant="destructive" type="button">
+                  Delete Account
+                </Button>
+              </AlertDialogTrigger>
+            </DeleteAlertBox>
+          </div>
+        </DialogContent>
+      </Dialog>
+    </>
+  );
+}
